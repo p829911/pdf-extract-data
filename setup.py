0e697f155df2c982efb45998dd6650ff0d425066
현승우 <p829911@gmail.com>
2019-05-21 19:11:53 +0900

    version update 1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdfextractdata",
-    version="1.7",
+    version="1.8",
     description="Python package for extracting data from pdf",
     author="p829911",
     author_email="p829911@gmail.com",
