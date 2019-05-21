27aac9e77fe200c75b3f8354911ca979109bf0a7
p829911 <p829911@gmail.com>
2019-05-21 15:51:16 +0900

    version 1.6

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pdf_extract_data",
-    version="1.4",
+    name="pdfextractdata",
+    version="1.6",
     description="Python package for extracting data from pdf",
     author="p829911",
     author_email="p829911@gmail.com",
-    url="https://github.com/p829911/pdf_extract_data",
-    install_requires=["pandas", "numpy", "pdfminer.six", "camelot-py[cv]"],
+    url="https://github.com/p829911/pdf-extract-data",
+    install_requires=["camelot-py[cv]"],
     packages=find_packages(),
     keywords=["pdf extract", "extract data from pdf", "extract from pdf"],
     python_requires=">=3"
