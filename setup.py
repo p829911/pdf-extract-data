d3910f30625012a02abc51a61e9df2c28ccf7bac
p829911 <p829911@gmail.com>
2019-05-21 14:15:20 +0900

    modified

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     description="Python package for extracting data from pdf",
     author="p829911",
     author_email="p829911@gmail.com",
-    url="https://github.com/p829911/pdf-extract-data",
+    url="https://github.com/p829911/pdf_extract_data",
     install_requires=["pandas", "numpy", "pdfminer.six", "camelot-py[cv]"],
     packages=find_packages(),
-    keyword=["pdf extract", "extract data from pdf", "extract from pdf"],
+    keywords=["pdf extract", "extract data from pdf", "extract from pdf"],
     python_requires=">=3"
 )
