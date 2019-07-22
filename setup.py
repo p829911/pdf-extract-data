c6e0123095c6cdff1a59a61680ac1cd09255d470
현승우 <p829911@gmail.com>
2019-07-22 23:39:11 +0900

    update versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdfextractdata",
-    version="4.2",
+    version="4.5",
     description="Python package for extracting data from pdf",
     author="p829911",
     author_email="p829911@gmail.com",
